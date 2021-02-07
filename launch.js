9d04800c1a0eba4199e6b72882ef7595d9beaaad
Hanprogramer <30620423+Hanprogramer@users.noreply.github.com>
2021-02-08 00:45:20 +0700

    Update launch.js

--- launch.js
+++ launch.js
@@ -1,10 +1,14 @@
 const {app, BrowserWindow} = require('electron');
 
+// Only create the window when electron is ready
 app.on("ready", ()=>{
+    // Create the window instance
     const window = new BrowserWindow({
         title : "CoreCoder:Studio dev preview 1",
         frame: false
     });
+
+    // Show the window then load the html file
     window.show();
     window.loadFile("./src/project-manager.html");
 });

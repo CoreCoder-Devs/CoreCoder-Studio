64042be7cdf896105f3e4505f622ac7603ff68d1
Hanprogramer <30620423+Hanprogramer@users.noreply.github.com>
2021-02-08 00:39:39 +0700

    Initial Commit

--- launch.js
+++ launch.js
@@ -2,7 +2,8 @@ const {app, BrowserWindow} = require('electron');
 
 app.on("ready", ()=>{
     const window = new BrowserWindow({
-        title : "CoreCoder:Studio dev preview 1"
+        title : "CoreCoder:Studio dev preview 1",
+        frame: false
     });
     window.show();
     window.loadFile("./src/project-manager.html");

839f6e72fa3a71c0a7472d43ba68fcc529096cd3
Hanprogramer <30620423+Hanprogramer@users.noreply.github.com>
2021-02-08 20:21:52 +0700

    Removed Unessecary code

--- src/project-manager.js
+++ src/project-manager.js
@@ -2,48 +2,7 @@ const electron = require("electron");
 const ipc = electron.ipcRenderer;
 const {settings} = require("./js/global_settings");
 
-function initTitleBar(){
-    /// TITLE BAR
-    /// Initialize the titlebar's click function
-    
-    let minimizeBtn = document.getElementById("tbBtnMin");
-    let maximizeBtn = document.getElementById("tbBtnMax");
-    let closeBtn = document.getElementById("tbBtnCls");
-
-    minimizeBtn.addEventListener("click", ()=>{
-        // Minimize
-        ipc.send("minimize");
-    });
-    maximizeBtn.addEventListener("click", ()=>{
-        if(maximizeBtn.classList.contains("maximizebtn")){
-            // Minimize
-            ipc.send("maximize");
-        }
-        else if(maximizeBtn.classList.contains("restorebtn")){
-            // Restore
-            ipc.send("restore");
-        }
-    });
-    closeBtn.addEventListener("click", ()=>{
-        // Minimize
-        ipc.send("close");
-    });
-
-    // Listens for windows event
-    ipc.on("windowStateMaximized", (event,args)=>{
-        let maximizeBtn = document.getElementById("tbBtnMax");
-        maximizeBtn.classList.remove("maximizebtn");
-        maximizeBtn.classList.add("restorebtn");
-    });
-    ipc.on("windowStateRestored", (event,args)=>{
-        let maximizeBtn = document.getElementById("tbBtnMax");
-        maximizeBtn.classList.add("maximizebtn");
-        maximizeBtn.classList.remove("restorebtn");
-    });
-}
-
 function init(){
-    initTitleBar();
     settings.GlobalSettings.lang = "cn";
     settings.localizeInterface();
 }

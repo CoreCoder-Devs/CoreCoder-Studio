765e2de731ce22a937f454d0ca4a58e41169cb80
skybird23333 <2603003199a@gmail.com>
2021-02-08 21:06:18 +0800

    Feat ProjectMan: make thing vue

--- src/project-manager.js
+++ src/project-manager.js
@@ -3,7 +3,9 @@ const ipc = electron.ipcRenderer;
 const {settings} = require("./js/global_settings");
 
 function initTitleBar(){
+    /// TITLE BAR
     /// Initialize the titlebar's click function
+    
     let minimizeBtn = document.getElementById("tbBtnMin");
     let maximizeBtn = document.getElementById("tbBtnMax");
     let closeBtn = document.getElementById("tbBtnCls");
@@ -45,3 +47,26 @@ function init(){
     settings.GlobalSettings.lang = "cn";
     settings.localizeInterface();
 }
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        //TITLE BAR
+        maximised: false,
+        ipc: ipc,
+        settings: settings
+    },
+
+    created() {
+        settings.GlobalSettings.lang = "cn";
+        settings.localizeInterface();
+        let vm = this
+        // Listens for windows event
+        ipc.on("windowStateMaximized", (event,args)=>{
+            vm.maximised = true
+        });
+        ipc.on("windowStateRestored", (event,args)=>{
+            vm.maximised = false
+        });
+    }
+  })

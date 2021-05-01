52376ba4e742270e78d9571b951e4829879670e4
Hanprogramer <han.programer@gmail.com>
2021-05-01 09:09:15 +0700

    Can now open recent projects

--- src/project-manager.js
+++ src/project-manager.js
@@ -6,9 +6,20 @@ const Projects = require('./js/projects')
 const imagesize = require('image-size')
 
 Vue.component('project', {
+    /**
+     * Summary:
+     * This component inherits template from a string, 
+     * the onclick function is running the "openRecentProject" function
+     * the argument is the 'project' as [object Object]
+     * containing name, path and uuid
+     */
     props: ['project'],
     template: `
-      <div class="project">
+      <div class="project" 
+        :data-project="JSON.stringify(project)" 
+        :onclick="'openRecentProject( JSON.parse( this.getAttribute(\\\'data-project\\\')) )'" 
+        :oncontextmenu="'removeRecentProject( JSON.parse( this.getAttribute(\\\'data-project\\\')) )'"
+      >
          <img style="float:left;" v-bind:src="project.iconPath" width="50" v-bind:style="{'filter': 'image-rendering: pixelated;'}">
          <h3>{{ project.name }}</h3>
          <label class="project-path">{{ project.uuid }}@{{ project.versionString }}</label><br>
@@ -20,16 +31,21 @@ Vue.component('project', {
             filter: true
         }
     },
+    methods: {
+        openRecentProject: function (evnt) {
+            console.log(evnt);
+        }
+    },
 
     created() {
-        if(this.project.iconPath) {
+        if (this.project.iconPath) {
             try {
                 const size = imagesize(this.project.iconPath)
-                if(size.width > 128 || size.height > 128) {
+                if (size.width > 128 || size.height > 128) {
                     this.filter = false
                     //TODO doesnt correctly add filters
                 }
-            } catch(e) {
+            } catch (e) {
                 console.log(e)
             }
         }
@@ -117,3 +133,41 @@ let app = new Vue({
 
     }
 })
+
+function openRecentProject(project) {
+    /**
+     * Project : {name,uuid,path}
+     */
+    let path = project.path;
+
+    if (path.includes("behavior_packs")) {
+        // Add to the localStorage
+        // if it is a behavior_packs
+        window.localStorage.setItem("bp_path", path);
+        // TODO:Auto detect pack dependencies
+        window.localStorage.removeItem("rp_path");
+    } else if (path.includes("resource_packs")) {
+        // Add to the localStorage
+        // if it is a resource_packs
+        window.localStorage.setItem("rp_path", path);
+        // TODO:Auto detect pack dependencies
+        window.localStorage.removeItem("bp_path");
+    } else {
+        // TODO:: detect the pack by looking at it's JSON files manifest.json
+    }
+
+
+    window.location = "./editor.html";
+}
+
+function removeRecentProject(project) {
+    /**
+     * Remove recently opened project from the list
+     */
+    let array = this.projects;
+    let index = array.indexOf(project);
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+    this.projects = array;
+}

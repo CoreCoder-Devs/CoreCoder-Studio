7912e234ed816845ce07cbac620769e0b37b4e4e
Hanprogramer <han.programer@gmail.com>
2021-05-14 15:01:30 +0700

    Zoom can't be smaller than 1

--- src/lib/pixydust/pixydust.js
+++ src/lib/pixydust/pixydust.js
@@ -357,7 +357,7 @@ function createEditorElm(instance, elm, imgPath = "") {
     function onmousescroll(ev) {
         // zooming functionality
         instance.zoom -= (ev.deltaY / 100.0) / 2.0;
-        instance.zoom = Math.min(Math.max(instance.zoom, 0.5), 40);
+        instance.zoom = Math.min(Math.max(instance.zoom, 1), 40);
         instance.drawCanvas(instance, rendererCanvas, ctx);
     }
 

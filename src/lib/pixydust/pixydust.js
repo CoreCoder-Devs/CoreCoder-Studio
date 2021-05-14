ceebfec3ae3cfa41c2f2af8d868aad2d16e7cd19
Hanprogramer <han.programer@gmail.com>
2021-05-14 15:00:17 +0700

    Cleanup PixyDust API

--- src/lib/pixydust/pixydust.js
+++ src/lib/pixydust/pixydust.js
@@ -5,8 +5,221 @@
  * written by Hanprogramer
  */
 
+/**
+ * 
+                                 Apache License
+                           Version 2.0, January 2004
+                        http://www.apache.org/licenses/
 
+   TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
+   1. Definitions.
+
+      "License" shall mean the terms and conditions for use, reproduction,
+      and distribution as defined by Sections 1 through 9 of this document.
+
+      "Licensor" shall mean the copyright owner or entity authorized by
+      the copyright owner that is granting the License.
+
+      "Legal Entity" shall mean the union of the acting entity and all
+      other entities that control, are controlled by, or are under common
+      control with that entity. For the purposes of this definition,
+      "control" means (i) the power, direct or indirect, to cause the
+      direction or management of such entity, whether by contract or
+      otherwise, or (ii) ownership of fifty percent (50%) or more of the
+      outstanding shares, or (iii) beneficial ownership of such entity.
+
+      "You" (or "Your") shall mean an individual or Legal Entity
+      exercising permissions granted by this License.
+
+      "Source" form shall mean the preferred form for making modifications,
+      including but not limited to software source code, documentation
+      source, and configuration files.
+
+      "Object" form shall mean any form resulting from mechanical
+      transformation or translation of a Source form, including but
+      not limited to compiled object code, generated documentation,
+      and conversions to other media types.
+
+      "Work" shall mean the work of authorship, whether in Source or
+      Object form, made available under the License, as indicated by a
+      copyright notice that is included in or attached to the work
+      (an example is provided in the Appendix below).
+
+      "Derivative Works" shall mean any work, whether in Source or Object
+      form, that is based on (or derived from) the Work and for which the
+      editorial revisions, annotations, elaborations, or other modifications
+      represent, as a whole, an original work of authorship. For the purposes
+      of this License, Derivative Works shall not include works that remain
+      separable from, or merely link (or bind by name) to the interfaces of,
+      the Work and Derivative Works thereof.
+
+      "Contribution" shall mean any work of authorship, including
+      the original version of the Work and any modifications or additions
+      to that Work or Derivative Works thereof, that is intentionally
+      submitted to Licensor for inclusion in the Work by the copyright owner
+      or by an individual or Legal Entity authorized to submit on behalf of
+      the copyright owner. For the purposes of this definition, "submitted"
+      means any form of electronic, verbal, or written communication sent
+      to the Licensor or its representatives, including but not limited to
+      communication on electronic mailing lists, source code control systems,
+      and issue tracking systems that are managed by, or on behalf of, the
+      Licensor for the purpose of discussing and improving the Work, but
+      excluding communication that is conspicuously marked or otherwise
+      designated in writing by the copyright owner as "Not a Contribution."
+
+      "Contributor" shall mean Licensor and any individual or Legal Entity
+      on behalf of whom a Contribution has been received by Licensor and
+      subsequently incorporated within the Work.
+
+   2. Grant of Copyright License. Subject to the terms and conditions of
+      this License, each Contributor hereby grants to You a perpetual,
+      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+      copyright license to reproduce, prepare Derivative Works of,
+      publicly display, publicly perform, sublicense, and distribute the
+      Work and such Derivative Works in Source or Object form.
+
+   3. Grant of Patent License. Subject to the terms and conditions of
+      this License, each Contributor hereby grants to You a perpetual,
+      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+      (except as stated in this section) patent license to make, have made,
+      use, offer to sell, sell, import, and otherwise transfer the Work,
+      where such license applies only to those patent claims licensable
+      by such Contributor that are necessarily infringed by their
+      Contribution(s) alone or by combination of their Contribution(s)
+      with the Work to which such Contribution(s) was submitted. If You
+      institute patent litigation against any entity (including a
+      cross-claim or counterclaim in a lawsuit) alleging that the Work
+      or a Contribution incorporated within the Work constitutes direct
+      or contributory patent infringement, then any patent licenses
+      granted to You under this License for that Work shall terminate
+      as of the date such litigation is filed.
+
+   4. Redistribution. You may reproduce and distribute copies of the
+      Work or Derivative Works thereof in any medium, with or without
+      modifications, and in Source or Object form, provided that You
+      meet the following conditions:
+
+      (a) You must give any other recipients of the Work or
+          Derivative Works a copy of this License; and
+
+      (b) You must cause any modified files to carry prominent notices
+          stating that You changed the files; and
+
+      (c) You must retain, in the Source form of any Derivative Works
+          that You distribute, all copyright, patent, trademark, and
+          attribution notices from the Source form of the Work,
+          excluding those notices that do not pertain to any part of
+          the Derivative Works; and
+
+      (d) If the Work includes a "NOTICE" text file as part of its
+          distribution, then any Derivative Works that You distribute must
+          include a readable copy of the attribution notices contained
+          within such NOTICE file, excluding those notices that do not
+          pertain to any part of the Derivative Works, in at least one
+          of the following places: within a NOTICE text file distributed
+          as part of the Derivative Works; within the Source form or
+          documentation, if provided along with the Derivative Works; or,
+          within a display generated by the Derivative Works, if and
+          wherever such third-party notices normally appear. The contents
+          of the NOTICE file are for informational purposes only and
+          do not modify the License. You may add Your own attribution
+          notices within Derivative Works that You distribute, alongside
+          or as an addendum to the NOTICE text from the Work, provided
+          that such additional attribution notices cannot be construed
+          as modifying the License.
+
+      You may add Your own copyright statement to Your modifications and
+      may provide additional or different license terms and conditions
+      for use, reproduction, or distribution of Your modifications, or
+      for any such Derivative Works as a whole, provided Your use,
+      reproduction, and distribution of the Work otherwise complies with
+      the conditions stated in this License.
+
+   5. Submission of Contributions. Unless You explicitly state otherwise,
+      any Contribution intentionally submitted for inclusion in the Work
+      by You to the Licensor shall be under the terms and conditions of
+      this License, without any additional terms or conditions.
+      Notwithstanding the above, nothing herein shall supersede or modify
+      the terms of any separate license agreement you may have executed
+      with Licensor regarding such Contributions.
+
+   6. Trademarks. This License does not grant permission to use the trade
+      names, trademarks, service marks, or product names of the Licensor,
+      except as required for reasonable and customary use in describing the
+      origin of the Work and reproducing the content of the NOTICE file.
+
+   7. Disclaimer of Warranty. Unless required by applicable law or
+      agreed to in writing, Licensor provides the Work (and each
+      Contributor provides its Contributions) on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+      implied, including, without limitation, any warranties or conditions
+      of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
+      PARTICULAR PURPOSE. You are solely responsible for determining the
+      appropriateness of using or redistributing the Work and assume any
+      risks associated with Your exercise of permissions under this License.
+
+   8. Limitation of Liability. In no event and under no legal theory,
+      whether in tort (including negligence), contract, or otherwise,
+      unless required by applicable law (such as deliberate and grossly
+      negligent acts) or agreed to in writing, shall any Contributor be
+      liable to You for damages, including any direct, indirect, special,
+      incidental, or consequential damages of any character arising as a
+      result of this License or out of the use or inability to use the
+      Work (including but not limited to damages for loss of goodwill,
+      work stoppage, computer failure or malfunction, or any and all
+      other commercial damages or losses), even if such Contributor
+      has been advised of the possibility of such damages.
+
+   9. Accepting Warranty or Additional Liability. While redistributing
+      the Work or Derivative Works thereof, You may choose to offer,
+      and charge a fee for, acceptance of support, warranty, indemnity,
+      or other liability obligations and/or rights consistent with this
+      License. However, in accepting such obligations, You may act only
+      on Your own behalf and on Your sole responsibility, not on behalf
+      of any other Contributor, and only if You agree to indemnify,
+      defend, and hold each Contributor harmless for any liability
+      incurred by, or claims asserted against, such Contributor by reason
+      of your accepting any such warranty or additional liability.
+
+   END OF TERMS AND CONDITIONS
+
+   APPENDIX: How to apply the Apache License to your work.
+
+      To apply the Apache License to your work, attach the following
+      boilerplate notice, with the fields enclosed by brackets "[]"
+      replaced with your own identifying information. (Don't include
+      the brackets!)  The text should be enclosed in the appropriate
+      comment syntax for the file format. We also recommend that a
+      file or class name and description of purpose be included on the
+      same "printed page" as the copyright notice for easier
+      identification within third-party archives.
+
+   Copyright [yyyy] [name of copyright owner]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+
+/**
+ * The pixy class
+ */
 class Pixy {
+    /**
+     * Construct a new Pixy editor instance, element can be accessed inside Pixy.elm
+     * @param {Node} elm the element to create Pixy on
+     * @param {String} imgPath path to the image
+     */
     constructor(elm, imgPath) {
         this.backgroundColor = "#292929";
         this.imgPath = imgPath;
@@ -23,27 +236,43 @@ class Pixy {
         this.canvas = null;
         this.rendererCanvas = null;
 
+        // For drawing
+        this.pixelImageData = null;
+        this.pixelData = null;
+
         this.elm = createEditorElm(this, elm, imgPath);
     }
 
-    setPixel(x, y, r, g, b) {
-        // var off = (y * this.imageData.width + x) * 4;
-        // var pixels = this.imageData.data;
-        // pixels[off] = r;
-        // pixels[off + 1] = g;
-        // pixels[off + 2] = b;
-        // pixels[off + 3] = 255;
-        // this.imageCanvas.getContext("2d").putImageData(this.imageData,0,0);
+    /**
+     * Set a pixel on the currently editing image
+     * @param {Number} x x coordinate
+     * @param {Number} y y coordinate
+     * @param {Number} r Red value
+     * @param {Number} g Green value
+     * @param {Number} b Blue value
+     * @param {Number} a Alpha value
+     */
+    setPixel(x, y, r, g, b, a) {
+        // Get context to draw
         var context = this.imageCanvas.getContext("2d");
-        var id = context.createImageData(1, 1); // only do this once per page
-        var d = id.data;                        // only do this once per page
-        d[0] = r;
-        d[1] = g;
-        d[2] = b;
-        d[3] = 255;
-        context.putImageData(id, x, y);
 
-        this.drawCanvas(this, this.rendererCanvas, this.rendererCanvas.getContext("2d"), this.elm);
+        // Create new data
+        if (this.pixelData == null || this.pixelImageData == null) {
+            this.pixelImageData = context.createImageData(1, 1); // only do this once per page
+            this.pixelData = this.pixelImageData.data;           // only do this once per page
+        }
+
+        // Set the pixel
+        this.pixelData[0] = r;
+        this.pixelData[1] = g;
+        this.pixelData[2] = b;
+        this.pixelData[3] = a;
+
+        // Put the pixel to the imageCanvas
+        context.putImageData(this.pixelImageData, x, y);
+
+        // Update the screen
+        this.drawCanvas(this, this.rendererCanvas, this.rendererCanvas.getContext("2d"));
     }
 
     /**
@@ -51,10 +280,9 @@ class Pixy {
      * @param {Pixy} instance The pixy instance
      * @param {Node} canvas the canvas element
      * @param {Context} ctx Context for drawing
-     * @param {Node} pixyElm the pixy DOM element
      */
-    drawCanvas(instance, canvas, ctx, pixyElm) {
-
+    drawCanvas(instance, canvas, ctx) {
+        // Set canvas's width and height to full size of the HTML element
         var rect = canvas.getBoundingClientRect();
         canvas.width = rect.width / instance.zoom;
         canvas.height = rect.height / instance.zoom;
@@ -67,7 +295,6 @@ class Pixy {
 
         // Draw the image
         ctx.drawImage(instance.imageCanvas, 0, 0);
-        // ctx.putImageData(instance.imageData, 0, 0);
     }
 }
 
@@ -96,10 +323,15 @@ function _generateElm() {
  * @returns The pixy editor element
  */
 function createEditorElm(instance, elm, imgPath = "") {
+    // Create the element
     var pixyElm = _generateElm();
+
+    // Set value on the instance
     instance.elm = pixyElm;
     instance.zoom = 2;
     instance.backgroundColor = '#393939';
+
+    // Get Drawing context
     var ctx = pixyElm.lastChild.getContext("2d");
 
     // turn off image aliasing
@@ -108,45 +340,50 @@ function createEditorElm(instance, elm, imgPath = "") {
     ctx.webkitImageSmoothingEnabled = false;
     ctx.imageSmoothingEnabled = false;
 
+    // Renderer canvas, to display on screen
     var rendererCanvas = pixyElm.lastChild;
     instance.rendererCanvas = rendererCanvas;
 
     rendererCanvas.onresize = function (ev) {
-        instance.drawCanvas(instance, rendererCanvas, ctx, pixyElm);
+        // When element resize, redraw
+        // Note: this event is inaccurate sometimes doesnt fire
+        instance.drawCanvas(instance, rendererCanvas, ctx);
     }
     window.addEventListener("resize", function (ev) {
-        instance.drawCanvas(instance, rendererCanvas, ctx, pixyElm);
+        // When window resize, redraw
+        instance.drawCanvas(instance, rendererCanvas, ctx);
     });
 
     function onmousescroll(ev) {
+        // zooming functionality
         instance.zoom -= (ev.deltaY / 100.0) / 2.0;
         instance.zoom = Math.min(Math.max(instance.zoom, 0.5), 40);
-        instance.drawCanvas(instance, rendererCanvas, ctx, pixyElm);
+        instance.drawCanvas(instance, rendererCanvas, ctx);
     }
 
     function ondraw(ev) {
-        // var imgCtx = instance.imageCanvas.getContext("2d");
-        // imgCtx.imageSmoothingEnabled = false;
-        // var r = 255, g = 200, b = 200, a = 255;
-        // imgCtx.fillStyle = "rgba(" + r + "," + g + "," + b + "," + (a / 255) + ")";
         var x = ev.layerX / instance.zoom;
         var y = ev.layerY / instance.zoom;
-        instance.setPixel(x, y, 255, 0, 0);
+        instance.setPixel(x, y, 255, 0, 0, 255);
     }
 
     rendererCanvas.addEventListener("mousewheel", function (ev) {
+        // Zooming on inside the canvas
         onmousescroll(ev);
     });
 
     pixyElm.addEventListener("mousewheel", function (ev) {
+        // Zooming on outside the canvas
         onmousescroll(ev);
     });
     pixyElm.addEventListener("mousedown", function (ev) {
+        // Mouse Down
         ondraw(ev);
     });
 
     pixyElm.addEventListener("mousemove", function (ev) {
-        if(ev.buttons > 0){
+        // Check if mouse dragged
+        if (ev.buttons > 0) {
             ondraw(ev);
         }
     });
@@ -172,7 +409,7 @@ function createEditorElm(instance, elm, imgPath = "") {
 
             // instance.imageData = rendererCanvas.createImageData(image.width, image.height);
             instance.imageData = instance.imageCanvas.getContext("2d").getImageData(0, 0, image.width, image.height);
-            instance.drawCanvas(instance, rendererCanvas, ctx, pixyElm);
+            instance.drawCanvas(instance, rendererCanvas, ctx);
         }
         image.src = imgPath;
     }
@@ -182,6 +419,12 @@ function createEditorElm(instance, elm, imgPath = "") {
     return pixyElm;
 }
 
+/**
+ * Create a brand new editor
+ * @param {Node} elm the element to create Pixy on
+ * @param {String} imgPath path to the image
+ * @returns {Pixy} the pixy editor instance
+ */
 function createEditor(elm, imgPath) {
     return new Pixy(elm, imgPath);
 }

785c01e6e8f27c6d662e1a2b772ea98ca58558fc
2603003199a <58811224+skybird23333@users.noreply.github.com>
2022-08-12 19:20:59 +0800

    Delete unused routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,9 @@
 import ComponentTest from "@/views/ComponentTest.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {
-            path: "/",
-            name: "home",
-            component: HomeView,
-        },
-        {
-            path: "/about",
-            name: "about",
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import("../views/AboutView.vue"),
-        },
         {
             path: "/comptest",
             name: "comptest",
